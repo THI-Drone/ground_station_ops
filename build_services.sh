7e9c82b314036ca6e892eea31e9870bd68d0104b
di-math <sidemuth705@googlemail.com>
2024-05-25 23:03:21 +0200

    Fixed typo.

--- build_services.sh
+++ build_services.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-ENV_FILE="./Ground_Station/web/fronted/.env"
-
-if [ ! -f "./Ground_Station/web/fronted/.env" ]; then
+if [ ! -f "Ground_Station/web/frontend/.env" ]; then
   echo "Error: .env for the frontend does not exist. Please create ./Ground_Station/web/frontend/.env containing VITE_BACKEND=192.168.0.101:80. Exiting."
   exit 1
 fi
 
-docker build -f ./Ground_Station/web/Dockerfile -t ground_station
-docker build -f ./Ground_Station_Bridge/Dockerfile -t ground_station_bridge
+docker build -t ground_station ./Ground_Station/web/frontend
+docker build -t ground_station_bridge ./Ground_Station_Bridge
