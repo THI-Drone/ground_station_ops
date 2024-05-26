e6ae74dea980a9f4ba7f63bc8c497fc092079213
di-math <sidemuth705@googlemail.com>
2024-05-26 11:43:15 +0200

    Edited startup script.

--- ground_station_startup.sh
+++ ground_station_startup.sh
@@ -2,6 +2,8 @@
 
 rm /tmp/thi_drone
 
-docker run -d --restart unless-stopped --network host -v /tmp/:/tmp/ ground_station_bridge
+docker run -d --network host -v /tmp/:/tmp/ ground_station_bridge
 
-docker run -d -p 80:3000 -v /tmp/:/tmp/ --restart unless-stopped ground_station
+sleep 2  # strangely it won't work otherwise 
+
+docker run -d -p 80:3000 -v /tmp/:/tmp/ ground_station
