60f9a7a0304b5fc5cf1ea5fe667230c4370d44fb
di-math <sidemuth705@googlemail.com>
2024-05-26 15:37:45 +0200

    Screw the startup script this takes way too much time rn.

--- build_services.sh
+++ build_services.sh
@@ -7,9 +7,3 @@ fi
 
 docker build -t ground_station ./Ground_Station
 docker build -t ground_station_bridge ./Ground_Station_Bridge
-
-docker run --restart unless-stopped -d --network host -v /tmp/:/tmp/ ground_station_bridge
-
-sleep 8  # strangely it won't work otherwise 
-
-docker run --restart unless-stopped -d -p 80:3000 -v /tmp/:/tmp/ ground_station
