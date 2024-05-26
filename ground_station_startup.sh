d742ccbc2a1a8d0ebf382ee0a50dc8c5d1738d93
di-math <sidemuth705@googlemail.com>
2024-05-26 14:57:28 +0200

    Add --rm to docker run in startup script.

--- ground_station_startup.sh
+++ ground_station_startup.sh
@@ -2,8 +2,8 @@
 
 rm /tmp/thi_drone
 
-docker run -d --network host -v /tmp/:/tmp/ ground_station_bridge
+docker run --rm -d --network host -v /tmp/:/tmp/ ground_station_bridge
 
 sleep 8  # strangely it won't work otherwise 
 
-docker run -d -p 80:3000 -v /tmp/:/tmp/ ground_station
+docker run --rm -d -p 80:3000 -v /tmp/:/tmp/ ground_station
