53a3147094be86323b252e08e8054e63591d48ca
Simon Demuth <sidemuth705@googlemail.com>
2024-06-12 10:33:02 +0200

    Edited docker run setup for the bridge

--- run_services.sh
+++ run_services.sh
@@ -4,4 +4,4 @@ rm /tmp/thi_drone
 
 docker run -d --rm -p 80:3000 -v /tmp/:/tmp/ ground_station
 
-docker run -d --rm --network host -v /tmp/:/tmp/ ground_station_bridge 
+docker run -d --rm --network host -v /tmp/:/tmp/ ground_station_bridge /bin/bash -c "source install/setup.bash && ros2 run telemetry_package telemetry_node --ros-args -r __ns:=/uav_4"
